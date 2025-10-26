84676be4e9675c629d1d4a9f126440da4c813b45
Marcus Lafaiete <marcuslaf@hotmail.com>
2025-10-26 17:57:48 -0300

    Primeiro commit - Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("=== Nokia ===");
+Smartphone nokia = new Nokia("11999887766", "Nokia G60", "111222333444555", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n=== iPhone ===");
+Smartphone iphone = new Iphone("11777889966", "iPhone 15", "999888777666555", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
